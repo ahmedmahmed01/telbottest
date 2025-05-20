7aaf9d3f3e05522bae5de18e5fe9c3d10bc9ecdd
sajjadnasiri <s.nasiri.cs@gmail.com>
2025-05-20 19:18:06 +0330

    fix event loop issue 03

--- test_bot.py
+++ test_bot.py
@@ -1,20 +1,26 @@
-import asyncio
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
+import nest_asyncio
+nest_asyncio.apply()
 
-TOKEN = os.getenv("BOT_TOKEN") or "توکن_اینجا"
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = "8087765766:AAHZP3brmuQn_L2fh42Iypm_AEtEDEEfKCM"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! من ربات async هستم.")
+    await update.message.reply_text("سلام! من یک ربات هستم.")
 
-async def run():
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
     print("✅ ربات در حال اجراست...")
     await app.run_polling()
 
-# بدون asyncio.run
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    import asyncio
+    asyncio.run(main())
